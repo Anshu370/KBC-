e56ed4b03f3c1a5d033dd7ca9a7b9999eff4d7b1
Anshu370 <77850344+Anshu370@users.noreply.github.com>
2021-05-04 19:44:38 +0530

    THIS FILE CONTAIN QUESTIONS

---
+++ text.py
@@ -0,0 +1,39 @@
+A = "\t\t\t(A).Which of the following was the first Indian state to issue photo identity cards to its voters?\n\t\t\t\t"
+a = {1: "Tamil Nadu", 2: "Rajasthan", 3: "West Bengal", 4: "Haryana"}
+CORRECT_ANSWER = a[4]
+
+B = "\t\t\t(B).Where in China was the coronavirus disease 2019, or COVID-19, first identified?\n\t\t\t\t"
+b = {1: "Shenzhou", 2: "Wuhan", 3: "Shanghai", 4: "Beijing"}
+CORRECT_ANSWER = b[2]
+
+C = "\t\t\t(C).Which place was merged with Dadra and Nagar Haveli to form India’s newest Union Territory?\n\t\t\t\t"
+c = {1: "Minicoy Island", 2: "Lakshadweep", 3: "Daman and Diu", 4: "Kutch"}
+CORRECT_ANSWER = c[3]
+
+D = "\t\t\t(D).Which is the first state the River Brahmaputra flows through on entering India?\n\t\t\t\t"
+d = {1: "Arunachal Pradesh", 2: "Nagaland", 3: "Assam", 4: "Meghalaya"}
+CORRECT_ANSWER = d[1]
+
+E = "\t\t\t(E).The world’s largest cricket stadium in Motera near Ahmedabad is named in honour of which leader?\n\t\t\t\t"
+e = {1: "Madhav Singh Solanki", 2: "Mahatma Gandhi", 3: "Morarji Desai", 4: "Sardar Patel"}
+CORRECT_ANSWER = e[3]
+
+F = "\t\t\t(F).What role did Mukesh Khanna play in B R Chopra’s TV series “Mahabharata”?\n\t\t\t\t"
+f = {1: "Bhima", 2: "Bhishma", 3: "Arjuna", 4: "Lord Krishna"}
+CORRECT_ANSWER = f[2]
+
+G = "\t\t\t(G).Which of the following heroines is a “Desi Girl”, according to a song featuring her, Abhishek Bachchan and John Abraham?\n\t\t\t\t"
+g = {1: "Anushka Sharma", 2: "Kareena Kapoor", 3: "Katrina Kaif", 4: "Priyanka Chopra"}
+CORRECT_ANSWER = g[4]
+
+H = "\t\t\t(H).Which of these is made of bone?\n\t\t\t\t"
+h = {1: "Elephant tusks", 2: "Deer antlers", 3: "Rhino horns", 4: "Walrus tusks"}
+CORRECT_ANSWER = g[2]
+
+I = "\t\t\t(I).Which colonial power ended its involvement in India by selling the rights of the Nicobar Islands to the British on October 18, 1868?\n\t\t\t\t"
+i = {1: "Denmark ", 2: "Italy ", 3: "France", 4: "Belgium "}
+CORRECT_ANSWER = i[1]
+
+J = "\t\t\t(J).Who commanded the ‘Hector’, the first British trading ship to land at Surat?\n\t\t\t\t"
+j = {1: "James Lancaster", 2: "Paul Canning", 3: "William Hawkins", 4: "Thomas Roe "}
+CORRECT_ANSWER = j[3]
